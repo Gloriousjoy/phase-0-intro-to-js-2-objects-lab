03993237783f0f04bcc9fc33fd034d3134c74d84
Gloriousjoy <gloriousfab99@gmail.com>
2022-08-12 10:37:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Glorious',
+    streetAddress: '23 California',
+}
+function updateEmployeeWithKeyAndValue () {
+    const updatedEmployee={...employee};
+    updatedEmployee['name'] = 'Sam';
+    updatedEmployee['streetAddress'] = '11 Broadway';
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue () {
+    employee ['name'] = 'Sam';
+    employee['streetAddress'] = '12 Broadway';
+    return employee;
+}
+function deleteFromEmployeeByKey () {
+    const employeeClone={...employee};
+    delete employeeClone.name;
+    return employeeClone;
+}
+function destructivelyDeleteFromEmployeeByKey () {
+    delete employee.name;
+    return employee;
+}
